c7eb485a22129471aa951490433ce4d69f7748fd
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2024-07-03 17:38:19 +0200

    Features FrontEnd

--- resources/views/components/features.blade.php
+++ resources/views/components/features.blade.php
@@ -1,16 +1,62 @@
-<div class="container flex flex-col gap-8 items-center">
-    <div class="pretitle ">
-        <p>Cómo trabajamos</p>
-    </div>
-    <h3 class="text-center">Convertimos sus ideas en realidad con un diseño gráfico y web único</h3>
-    <div class="flex flex-col md:flex-row pt-12 gap-12">
-        @foreach ($features as $feature)
-            <div class="flex flex-col items-center justify-between gap-8 text-center w-full md:w-1/3">
-                <i class="text-4xl {{ $feature->image }} text-primary   "></i>
-                <h4 class="">{{ $feature->title }}</h4>
-                <p>{{ $feature->description }}</p>
-                <button>Ver más</button>
+<div class="container flex flex-col gap-8">
+    <h3 class="text-center">Nuestros servicios incluyen</h3>
+    <div class="flex flex-col md:flex-row">
+        <div class="w-full md:w-1/2 flex flex-col pt-12 gap-8">
+            @foreach ($features as $feature)
+                <div class="w-full flex items-start gap-4">
+                    <img src="{{ $feature->image }}" alt="{{ $feature->title }}" class="max-w-5 pt-2">
+                    <div class="flex flex-col gap-4">
+                        <h4 class="">{{ $feature->title }}</h4>
+                        <p class="text-slate-400">{{ $feature->description }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="w-full md:w-1/2 flex flex-col pt-12 gap-8 items-center">
+            <h4 class="uppercase text-center">¿Tiene una urgencia? Contáctenos.</h4>
+            <div class="flex text-2xl font-bold">
+                <a href="#">
+                    <div class="btn-urgencies">
+                    <i class="fi fi-rr-phone-call"></i>
+                        {{ __('Urgencias') }}
+                    </div>
+                </a>
             </div>
-        @endforeach
+            <div class="flex items-center gap-3 md:gap-6">
+                <div class="w-1/3 gap-y-4 flex flex-col">
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/aire-acondicionado.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/fontaneria-2.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/electricidad-2.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                </div>
+                <div class=" w-1/3 gap-y-4 flex flex-col">
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/fontaneria.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/electricidad-2.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/aire-acondicionado-2.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                </div>
+                <div class=" w-1/3 gap-y-4 flex flex-col">
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/reformas.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/aire-acondicionado-3.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="overflow-hidden rounded-lg">
+                        <img src="{{ asset('storage/img/electricidad.webp') }}" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
